c41f7d1321f817815bad4db094f998ab9495e546
Deepak Saini <sainidipu@gmail.com>
2017-06-29 10:50:20 +0530

    Intro Screen

--- src/components/WalkThroughThird/WalkThroughThird.js
+++ src/components/WalkThroughThird/WalkThroughThird.js
@@ -3,17 +3,39 @@ import { View, Image, Text } from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
 import styles from './WalkThroughThirdStyles';
 import { Images } from '../../theme';
+import { Button } from 'native-base';
+import { Actions as Navigation } from 'react-native-router-flux';
 
 export default class WalkThroughThird extends Component {
   render() {
     return (
       <LinearGradient colors={['#FC214F','#D32735']} style={styles.linearGradient}>
-        <View style={styles.logo}>
+        <View style={styles.bag}>
+          <Image source={ Images.logo } />
           <Image source={ Images.mobilelogo } />
-          <Text style={styles.introText}>Track real time location of your trip.{'\n'}
-            <Text style={styles.introText}>Share your trip with friends and family.</Text>
-          </Text>
-          <Image source={ Images.introLocation }/>
+          <View style={styles.loginSection}>
+            <Text style={styles.introText}>Track real time location of your trip.{'\n'}
+              <Text style={styles.introText}>Share your trip with friends and family.</Text>
+            </Text>
+
+            <Button
+              rounded
+              style={{
+                backgroundColor:'#FFF',
+                margin: 20,
+                height:60,
+                width: 200,
+                justifyContent:'center',
+                alignSelf:'center'
+              }}
+              onPress={Navigation.home}
+            >
+              <Text style={styles.registerButtonText}>Register</Text>
+            </Button>
+            <Button transparent style={{alignSelf:'center'}}>
+              <Text style={styles.introText}>Click Me! </Text>
+            </Button>
+          </View>
         </View>
       </LinearGradient>
     );
--- src/components/WalkThroughThird/WalkThroughThirdStyles.js
+++ src/components/WalkThroughThird/WalkThroughThirdStyles.js
@@ -6,7 +6,7 @@ export default StyleSheet.create({
   linearGradient: {
     flex: 1
   },
-  logo: {
+  bag: {
     //marginTop: 100,
     paddingTop: 60,
     paddingBottom : 60,
@@ -15,10 +15,27 @@ export default StyleSheet.create({
     flexDirection: 'column',
     justifyContent: 'space-between',
   },
+  loginSection: {
+    alignItems: 'center',
+  },
   introText: {
     backgroundColor:'transparent',
     color:'#fff',
     textAlign:'center',
     fontSize: 18
+  },
+  registerButton: {
+    backgroundColor:'#FFF',
+    marginTop: 20,
+    height:60,
+    width: 200,
+    justifyContent:'center',
+    alignSelf:'center'
+  },
+  registerButtonText : {
+    backgroundColor:'transparent',
+    color:'#000',
+    textAlign:'center',
+    fontSize: 18,
   }
 });

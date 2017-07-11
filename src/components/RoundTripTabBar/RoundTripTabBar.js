bbbd9ce8f6ae06dbbf5449afd9d794fb2811fe82
Deepak Saini <sainidipu@gmail.com>
2017-07-11 09:55:39 +0530

    changes

--- src/components/RoundTripTabBar/RoundTripTabBar.js
+++ src/components/RoundTripTabBar/RoundTripTabBar.js
@@ -1,70 +1,65 @@
 import React, { PropTypes } from 'react';
 import {
-    StyleSheet,
-    Text,
-    View,
-    TouchableOpacity,
-    Image,
-    Alert
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  Image,
+  Alert
 } from 'react-native';
 import { Colors, Images, Metrics } from '../../theme';
 import styles from './RoundTripTabBarStyle';
 
 
 const RoundTripTabBar = React.createClass({
-    tabIcons: [],
-    propTypes: {
-        goToPage: React.PropTypes.func,
-        activeTab: React.PropTypes.number,
-        tabs: React.PropTypes.array,
-    },
+  tabIcons: [],
+  propTypes: {
+    goToPage: React.PropTypes.func,
+    activeTab: React.PropTypes.number,
+    tabs: React.PropTypes.array,
+  },
+  contextTypes : {
+    store: PropTypes.object,
+  },
+  componentDidMount() {
+    this._listener = this.props.scrollValue.addListener(this.setAnimationValue);
+  },
+  setAnimationValue({ value, }) {
+    this.tabIcons.forEach((icon, i) => {
+      const progress = Math.min(1, Math.abs(value - i));
+      icon.setNativeProps({
+        style: {
+          color: this.iconColor(progress),
+        },
+      });
+    });
+  },
+  //color between rgb(59,89,152) and rgb(204,204,204)
+  iconColor(progress) {
+    const red = 59 + (204 - 59) * progress;
+    const green = 89 + (204 - 89) * progress;
+    const blue = 152 + (204 - 152) * progress;
+    return `rgb(${red}, ${green}, ${blue})`;
+  },
 
-    contextTypes : {
-        store: PropTypes.object,
-    },
-
-    componentDidMount() {
-        this._listener = this.props.scrollValue.addListener(this.setAnimationValue);
-    },
-
-    setAnimationValue({ value, }) {
-        this.tabIcons.forEach((icon, i) => {
-            const progress = Math.min(1, Math.abs(value - i));
-            icon.setNativeProps({
-                style: {
-                    color: this.iconColor(progress),
-                },
-            });
-        });
-    },
-
-    //color between rgb(59,89,152) and rgb(204,204,204)
-    iconColor(progress) {
-        const red = 59 + (204 - 59) * progress;
-        const green = 89 + (204 - 89) * progress;
-        const blue = 152 + (204 - 152) * progress;
-        return `rgb(${red}, ${green}, ${blue})`;
-    },
-
-    render() {
-        const { activeTab, tabTitleText } = this.props;
-        console.log('Active tab : ',this.props.activeTab);
-        return <View style={[styles.tabs, this.props.style, ]}>
-            {this.props.tabs.map((tab, i) => {
-                return <TouchableOpacity
-                    key={tab}
-                    onPress={() => this.props.goToPage(i)}
-                    style={[styles.tab, {borderBottomWidth: 3, borderColor: i === activeTab ? Colors.white : Colors.transparent }]}
-                >
-                    <Text
-                        style={[styles.tabTextStyle, {color: i === activeTab ? Colors.white :  Colors.unselectedTabBarTextColor}]}
-                    >
-                        {tabTitleText[i]}
-                    </Text>
-                </TouchableOpacity>;
-            })}
-        </View>;
-    },
+  render() {
+    const { activeTab, tabTitleText } = this.props;
+    console.log('Active tab : ',this.props.activeTab);
+    return <View style={[styles.tabs, this.props.style, ]}>
+      {this.props.tabs.map((tab, i) => {
+        return <TouchableOpacity
+          key={tab}
+          onPress={() => this.props.goToPage(i)}
+          style={[styles.tab, {borderBottomWidth: 3, borderColor: i === activeTab ? Colors.white : Colors.transparent }]}>
+            <Text
+              style={[styles.tabTextStyle, {color: i === activeTab ? Colors.white :  Colors.unselectedTabBarTextColor}]}
+            >
+              {tabTitleText[i]}
+              </Text>
+        </TouchableOpacity>;
+      })}
+    </View>;
+  },
 });
 
 
--- src/containers/InviteFriend/InviteFriend.js
+++ src/containers/InviteFriend/InviteFriend.js
@@ -1,89 +1,91 @@
 import React, { Component } from 'react';
-import { View, Text, TouchableOpacity, Image, TextInput } from 'react-native';
-import { Container, Content, Icon, List, ListItem, Header, Button, Left, Body, Title, Right, Item, Input, Row, Col } from 'native-base';
-import styles from './InviteFriendStyle';
+import { Text, Image, TextInput } from 'react-native';
+import { Container, Content, Header, Button, Left, Body, Title, Right, Row, Col } from 'native-base';
 import { Colors, Images, Fonts } from '../../theme';
 import { Actions } from 'react-native-router-flux';
 import CustomMultiPicker from "react-native-multiple-select-list";
 
 const userList = {
-    "123":"Tom",
-    "124":"Michael",
-    "125":"Christin"
+  "123":"Tom",
+  "124":"Michael",
+  "125":"Christin"
 }
 
 export default class InviteFriend extends Component {
-    constructor() {
-        super();
-        this.state = {
-
-        }
+  constructor() {
+    super();
+    this.state = {
     }
+  }
+  onPressSearchList = () =>{
 
-    onPressSearchList = () =>{
-
-    };
-
-    render() {
-        return(
-            <Container style={{ backgroundColor: Colors.base }}>
-                <Header style={{backgroundColor:'#fc214f',borderBottomWidth:0}}>
-                    <Left/>
-                    <Body>
-                    <Title style={{color:'white'}}>Invite Friends</Title>
-                    </Body>
-                    <Right>
-                        <Right>
-                            <Button transparent onPress={()=> Actions.pop()}>
-                                <Text style={{ color: Colors.white, fontSize: Fonts.size.medium, fontFamily: Fonts.lato.bold}}>Cancel</Text>
-                            </Button>
-                        </Right>
-                    </Right>
-                </Header>
-                <Row style={{ flex: 1, height: 40, marginLeft: 15, marginTop: 15, marginRight: 15}}>
-                    <Col style={{ flex: 0.85, height: 40 }}>
-                        <Row style={{ height: 40, borderColor: Colors.settingHeadingTextColor, borderWidth: 1, flex: 1, borderRadius: 4, backgroundColor: Colors.white }}>
-                            <Col style={{ flex: 0.92 }}>
-                            <TextInput
-                                style={{ flex: 1, backgroundColor: Colors.white }}
-                                onChangeText={(text) => this.setState({text})}
-                                placeholderTextColor = {{ color: Colors.settingHeadingTextColor}}
-                                value={this.state.text}
-                            />
-                            </Col>
-                           <Col style={{ flex: 0.08, alignItems: 'center', justifyContent: 'center', marginRight: 10 }}>
-                            <Image style={{}} source={Images.searchicon} />
-                           </Col>
-                        </Row>
-                    </Col>
-                    <Col style={{ flex: 0.15, height: 40 }}>
-                        <Button transparent onPress={this.onPressSearchList()}>
-                            <Image style={{}} source={Images.inviteaddfriendicon} />
-                        </Button>
-                    </Col>
-                </Row>
-                <Content>
-                    <CustomMultiPicker
-                        options={userList}
-                        search={false} // should show search bar?
-                        multiple={true} //
-                        placeholder={"Search"}
-                        placeholderTextColor={'#757575'}
-                        returnValue={"label"} // label or value
-                        callback={(res)=>{ console.log(res) }} // callback, array of selected items
-                        rowBackgroundColor={"#eee"}
-                        rowHeight={40}
-                        rowRadius={5}
-                        iconColor={"#00a2dd"}
-                        iconSize={30}
-                        selectedIconName={"ios-checkmark-circle-outline"}
-                        unselectedIconName={"ios-radio-button-off-outline"}
-                        scrollViewHeight={130}
-                        selected={[1,2]} // list of options which are selected by default
-                    />
-
-                </Content>
-            </Container>
-        )
-    }
+  };
+  render() {
+    return(
+      <Container style={{ backgroundColor: Colors.base }}>
+        <Header style={{backgroundColor:'#fc214f',borderBottomWidth:0}}>
+          <Left/>
+          <Body>
+            <Title style={{color:'white'}}>Invite Friends</Title>
+          </Body>
+          <Right>
+            <Button transparent onPress={()=> Actions.pop()}>
+              <Text style={{ color: Colors.white, fontSize: Fonts.size.medium, fontFamily: Fonts.lato.bold}}>
+                Cancel
+              </Text>
+            </Button>
+          </Right>
+        </Header>
+        <Row style={{ flex: 1, height: 40, marginLeft: 15, marginTop: 15, marginRight: 15}}>
+          <Col style={{ flex: 0.85, height: 40 }}>
+            <Row
+              style={{ height: 40,
+                borderColor: Colors.settingHeadingTextColor,
+                borderWidth: 1,
+                flex: 1,
+                borderRadius: 4,
+                backgroundColor: Colors.white
+              }}>
+              <Col style={{ flex: 0.92 }}>
+                <TextInput
+                  style={{ flex: 1, backgroundColor: Colors.white }}
+                  onChangeText={(text) => this.setState({text})}
+                  placeholderTextColor = {{ color: Colors.settingHeadingTextColor}}
+                  value={this.state.text}
+                />
+              </Col>
+              <Col style={{ flex: 0.08, alignItems: 'center', justifyContent: 'center', marginRight: 10 }}>
+                <Image style={{}} source={Images.searchicon} />
+              </Col>
+            </Row>
+          </Col>
+          <Col style={{ flex: 0.15, height: 40 }}>
+            <Button transparent onPress={this.onPressSearchList()}>
+              <Image style={{}} source={Images.inviteaddfriendicon} />
+            </Button>
+          </Col>
+        </Row>
+        <Content>
+          <CustomMultiPicker
+            options={userList}
+            search={false} // should show search bar?
+            multiple={true} //
+            placeholder={"Search"}
+            placeholderTextColor={'#757575'}
+            returnValue={"label"} // label or value
+            callback={(res)=>{ console.log(res) }} // callback, array of selected items
+            rowBackgroundColor={"#eee"}
+            rowHeight={40}
+            rowRadius={5}
+            iconColor={"#00a2dd"}
+            iconSize={30}
+            selectedIconName={"ios-checkmark-circle-outline"}
+            unselectedIconName={"ios-radio-button-off-outline"}
+            scrollViewHeight={130}
+            selected={[1,2]} // list of options which are selected by default
+          />
+        </Content>
+      </Container>
+    );
+  }
 }

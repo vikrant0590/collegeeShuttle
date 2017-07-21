5143aabb41e7fba46b3d716ea82a0e70d645d454
Deepak Saini <Admin@Deepaks-Mac-mini.local>
2017-07-21 15:22:30 +0530

    tab icon styles fixed

--- src/TabIcon.js
+++ src/TabIcon.js
@@ -1,6 +1,7 @@
-import React, { PropTypes } from 'react';
-import { View, Text, Image } from 'react-native';
+import React,{ Component } from 'react';
+import { View, Text, Image, StyleSheet } from 'react-native';
 import { Colors, Fonts } from './theme';
+import PropTypes from 'prop-types';
 const propTypes = {
   selected: PropTypes.bool,
   title: PropTypes.string,
@@ -8,28 +9,38 @@ const propTypes = {
   selectedTabIcon: PropTypes.any
 };
 
-const TabIcon = (props) => (
-  <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-    <Image
-      style={{
-        marginBottom: 2,
-        marginTop: 2,
-        width: 32,
-        height: 32,
-        resizeMode: 'contain'
-      }}
-      source={props.selected ? props.selectedTabIcon : props.tabIcon} />
-    <Text
-      style={{
-        color: props.selected ? Colors.placeholderTextColor : Colors.profileInputHeadingColor,
-        fontFamily:Fonts.lato.base,
-        fontSize:Fonts.size.tab
-      }}>
-      {props.title}
-    </Text>
-  </View>
-);
+export default class TabIcon extends Component{
+
+  render() {
+    return(
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Image
+          style={styles.tabIcon}
+          source={this.props.selected ? this.props.selectedTabIcon : this.props.tabIcon} />
+        <Text
+          style={[styles.tabText,
+            {color: this.props.selected ? Colors.placeholderTextColor : Colors.profileInputHeadingColor}]}>
+          {this.props.title}
+        </Text>
+      </View>
+    )
+  }
+
+}
+
+const styles = StyleSheet.create({
+  tabIcon: {
+    marginBottom: 2,
+    marginTop: 2,
+    width: 32,
+    height: 32,
+    resizeMode: 'contain'
+  },
+  tabText: {
+    fontFamily:Fonts.lato.base,
+    fontSize:Fonts.size.tab
+  },
+});
+
 
 TabIcon.propTypes = propTypes;
-
-export default TabIcon;

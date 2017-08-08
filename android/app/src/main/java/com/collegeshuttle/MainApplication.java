08bf617d6b1259060c61069e3d4c311e9966b9c0
KensJaswal <Kens-Data@KensJaswals-Mac-mini.local>
2017-08-08 17:48:04 +0530

    Logout

--- android/app/src/main/java/com/collegeshuttle/MainApplication.java
+++ android/app/src/main/java/com/collegeshuttle/MainApplication.java
@@ -38,7 +38,6 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new RNGoogleSigninPackage(),
             new MapsPackage(),
             new LinearGradientPackage(),
             new VectorIconsPackage(),

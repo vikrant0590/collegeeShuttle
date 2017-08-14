2ba8026f587504059dd15934a07ac5f37eacf7d7
Deepak Saini <sainidipu@gmail.com>
2017-08-14 15:10:52 +0530

    merging

--- src/components/Calender/Calender.js
+++ src/components/Calender/Calender.js
@@ -14,12 +14,19 @@ export default class Calender extends Component {
       datesMarked: [],
       minDate: undefined,
       maxDate: undefined,
+      lastDate:undefined,
       calenderlist: {}
     };
     this.onDayPress = this.selecionarData.bind(this);
   }
 
   selecionarData(date){
+    const obj = {};
+    let {year, month, day} = date;
+    let datelist = [];
+    let minDate, maxDate = undefined;
+    let lastDate = this.state.lastDate;
+
     let style = [{ startingDay: true, color: Colors.selectedCalenderDateColor, textColor: Colors.white }];
     let midstyle = [{color: Colors.selectedCalenderDateColor, textColor: Colors.white }];
     let endstyle = [{endingDay: true, color: Colors.selectedCalenderDateColor, textColor: Colors.white}];
@@ -27,178 +34,65 @@ export default class Calender extends Component {
       { startingDay: true, color: Colors.selectedCalenderDateColor, textColor: Colors.white },
       {endingDay: true, color: Colors.selectedCalenderDateColor, textColor: Colors.white}
     ];
-    const obj = {};
-    let {year, month, day} = date;
-    let datelist = [];
 
-    if (this.state.minDate && this.state.maxDate) {
-      if (this.state.minDate >= day) {
-        this.setState({
-          minDate: day
-        },() => {
-          if (this.state.minDate !== undefined && this.state.maxDate !== undefined) {
-            minkey = `${ year }-${ month }-${ this.state.minDate }`;
-            maxkey = `${ year }-${ month }-${ this.state.maxDate }`;
-          }
-          if (minkey !== undefined && maxkey !== undefined) {
-            const a = moment(minkey,"YYYY-MM-DD");
-            const b = moment(maxkey,"YYYY-MM-DD");
-            if(a < b) {
-              for (const m = moment(a); m.isAfter(b, 'days') <= 0; m.add(1, 'days')) {
-                datelist.push(m.format("YYYY-MM-DD"));
-              }
-            }else {
-              for (let m = moment(b); m.isSameOrBefore(a); m.add(1, 'day')) {
-                datelist.push(m.format("YYYY-MM-DD"));
-              }
-            }
-          }
-
-          for (let i = 0; i < datelist.length; i++) {
-            if (i === 0) {
-              obj[datelist[0]] = style;
-              if(datelist.length - 1 === 0){
-                obj[datelist[0]] = onlyOneStyle;
-              }
-            } else if (i === datelist.length - 1) {
-              obj[datelist[datelist.length - 1]] = endstyle;
-            }
-            else {
-              obj[datelist[i]] = midstyle;
-            }
-          }
-          this.setState({calenderlist: obj});
-        });
-      } else {
-        this.setState({
-          maxDate: day
-        },() =>{
-          if (this.state.minDate !== undefined && this.state.maxDate !== undefined) {
-            minkey = `${ year }-${ month }-${ this.state.minDate }`;
-            maxkey = `${ year }-${ month }-${ this.state.maxDate }`;
-          }
-          if (minkey !== undefined && maxkey !== undefined) {
-            const a = moment(minkey,"YYYY-MM-DD");
-            const b = moment(maxkey,"YYYY-MM-DD");
-            if(a < b) {
-              for (const m = moment(a); m.isAfter(b, 'days') <= 0; m.add(1, 'days')) {
-                datelist.push(m.format("YYYY-MM-DD"));
-              }
-            }else {
-              for (let m = moment(b); m.isSameOrBefore(a); m.add(1, 'day')) {
-                datelist.push(m.format("YYYY-MM-DD"));
-              }
-            }
-          }
-
-          for (let i = 0; i < datelist.length; i++) {
-            if (i === 0) {
-              obj[datelist[0]] = style;
-              if(datelist.length - 1 === 0){
-                obj[datelist[0]] = onlyOneStyle;
-              }
-            } else if (i === datelist.length - 1) {
-              obj[datelist[datelist.length - 1]] = endstyle;
-            }
-            else {
-              obj[datelist[i]] = midstyle;
-            }
-          }
-          this.setState({calenderlist: obj});
-        });
-      }
-    } else if ( this.state.minDate && this.state.maxDate === undefined ) {
-      if (this.state.minDate >= day) {
-        let tempMindate = this.state.minDate;
-        let tempMaxdate = day;
-        let temp = 0;
-        temp = tempMindate;
-        tempMindate = day;
-        tempMaxdate = temp;
-        this.setState({
-          minDate: tempMindate,
-          maxDate: tempMaxdate
-        },() =>{
-          if (this.state.minDate !== undefined && this.state.maxDate !== undefined) {
-            minkey = `${ year }-${ month }-${ this.state.minDate }`;
-            maxkey = `${ year }-${ month }-${ this.state.maxDate }`;
-          }
-          if (minkey !== undefined && maxkey !== undefined) {
-            const a = moment(minkey,"YYYY-MM-DD");
-            const b = moment(maxkey,"YYYY-MM-DD");
-            if(a < b) {
-              for (const m = moment(a); m.isAfter(b, 'days') <= 0; m.add(1, 'days')) {
-                datelist.push(m.format("YYYY-MM-DD"));
-              }
-            }else {
-              for (let m = moment(b); m.isSameOrBefore(a); m.add(1, 'day')) {
-                datelist.push(m.format("YYYY-MM-DD"));
-              }
-            }
-          }
-
-          for (let i = 0; i < datelist.length; i++) {
-            if (i === 0) {
-              obj[datelist[0]] = style;
-              if(datelist.length - 1 === 0){
-                obj[datelist[0]] = onlyOneStyle;
-              }
-            } else if (i === datelist.length - 1) {
-              obj[datelist[datelist.length - 1]] = endstyle;
-            }
-            else {
-              obj[datelist[i]] = midstyle;
-            }
-          }
-          this.setState({calenderlist: obj});
-        });
-      } else {
-        this.setState({
-          maxDate: day
-        },() => {
-          if (this.state.maxDate !== undefined) {
-            maxkey = `${ year }-${ month }-${ this.state.maxDate }`;
-            minkey = `${ year }-${ month }-${ this.state.minDate }`;
-          }
-          if (minkey !== undefined && maxkey !== undefined) {
-            const a = moment(minkey,"YYYY-MM-DD");
-            const b = moment(maxkey,"YYYY-MM-DD");
-            if(a < b) {
-              for (const m = moment(a); m.isAfter(b, 'days') <= 0; m.add(1, 'days')) {
-                datelist.push(m.format("YYYY-MM-DD"));
-              }
-            }else {
-              for (let m = moment(b); m.isSameOrBefore(a); m.add(1, 'day')) {
-                datelist.push(m.format("YYYY-MM-DD"));
-              }
-            }
-          }
-
-          for (let i = 0; i < datelist.length; i++) {
-            if (i === 0) {
-              obj[datelist[0]] = style;
-              if(datelist.length - 1 === 0){
-                obj[datelist[0]] = onlyOneStyle;
-              }
-            } else if (i === datelist.length - 1) {
-              obj[datelist[datelist.length - 1]] = endstyle;
-            }
-            else {
-              obj[datelist[i]] = midstyle;
-            }
-          }
-          this.setState({calenderlist: obj});
-        })
-      }
-    } else {
+    if ((this.state.minDate === undefined && this.state.minDate === undefined) || day === this.state.minDate || day === lastDate){
+      minDate = maxDate = day;
+      this.setState({
+        minDate: day,
+        maxDate: day
+      });
+    } else if (day > this.state.minDate && (day < this.state.maxDate || day > this.state.maxDate)){ 
+      minDate = this.state.minDate;
+      maxDate = day;
+      this.setState({
+        maxDate: day
+      });
+    } else if((this.state.minDate === this.state.maxDate || this.state.maxDate === this.state.maxDate) && day < this.state.minDate){
+      minDate = day;
+      maxDate = this.state.maxDate;
       this.setState({
         minDate: day
-      },() => {
-        if (this.state.minDate !== undefined) {
-          minkey = `${ year }-${ month }-${ this.state.minDate }`;
-        }
       });
     }
+
+    if (minDate !== undefined && maxDate !== undefined) {
+      minkey = `${ year }-${ month }-${ minDate }`;
+      maxkey = `${ year }-${ month }-${ maxDate }`;
+    }
+
+    if (minkey !== undefined && maxkey !== undefined) {
+      const a = moment(minkey,"YYYY-MM-DD");
+      const b = moment(maxkey,"YYYY-MM-DD");
+      if(a < b) {
+        for (const m = moment(a); m.isAfter(b, 'days') <= 0; m.add(1, 'days')) {
+          datelist.push(m.format("YYYY-MM-DD"));
+        }
+      } else {
+        for (let m = moment(b); m.isSameOrBefore(a); m.add(1, 'day')) {
+          datelist.push(m.format("YYYY-MM-DD"));
+        }
+      }
+    }
+
+    for (let i = 0; i < datelist.length; i++) {
+      if (i === 0) {
+        obj[datelist[0]] = style;
+        if(datelist.length - 1 === 0){
+          obj[datelist[0]] = onlyOneStyle;
+        }
+      } else if (i === datelist.length - 1) {
+        obj[datelist[datelist.length - 1]] = endstyle;
+      }
+      else {
+        obj[datelist[i]] = midstyle;
+      }
+    }
+
+    this.setState({
+      lastDate: day,
+      calenderlist: obj
+    });
+    return false;
   }
 
 
